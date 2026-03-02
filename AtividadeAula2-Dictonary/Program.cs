d822f104ebee8c6438850760fface25dd3bf8630
IsabelleToricelli <isabelletoricelli68@gmail.com>
2026-03-02 10:09:54 -0300

    Realização da atividade de dicionário

---
+++ AtividadeAula2-Dictonary/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Criar um dicionario para armazenar telefone
+            Dictionary<String, int> listaTelefonica = new Dictionary<String, int>();
+            //Adicionar 3 telefones 
+            listaTelefonica.Add("Mario", 1193377980);
+            listaTelefonica.Add("Isabelle", 1197860000);
+            listaTelefonica.Add("Carina", 1198081120);
+
+            //Buscar um telefone pelo nome
+            Console.WriteLine("Digite o nome para buscar o telefone: ");
+            String nome = Console.ReadLine();
+
+            //Usar trygetvalue para verificar se o nome existe no dicionário e obter o telefone correspondente
+            if (listaTelefonica.TryGetValue(nome, out int telefone))
+            {
+                Console.WriteLine($"O telefone de {nome} é: {telefone}");
+            }
+            else
+            {
+                Console.WriteLine($"O nome {nome} não foi encontrado na lista telefônica.");
+            }
+        }
+    }
+}
