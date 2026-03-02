476c6ec319407c138eeae88d6efda910bc02489c
IsabelleToricelli <isabelletoricelli68@gmail.com>
2026-03-02 09:31:17 -0300

    Resolução do exercicio de Listas

---
+++ Aula2-ListGenerics/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Criacao de uma lista de produtos 
+            List<string> list = new List<string>();
+            //Criar 5 objetos 
+            list.Add("Coca-Cola");
+            list.Add("Pepsi");
+            list.Add("Fanta");
+            list.Add("Sprite");
+            list.Add("Guaraná");
+
+            //Mostrar os produtos da lista
+            Console.WriteLine("Lista de Produtos");
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+            //Remover um produto 
+            list.Remove("Pepsi");
+
+            //Mostrar a lista atualizada
+            Console.WriteLine("Lista de Produtos atualizada");
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
